a6646c7038f15679fd34d9845738a9de3c9f2095
whatwewant <tobewhatwewant@outlook.com>
2023-08-21 01:34:04 +0800

    test: add test for LoadLocalesFromURL

--- utils_test.go
+++ utils_test.go
@@ -39,3 +39,20 @@ func TestLoadLocalesFromDir(t *testing.T) {
 	testify.Equal(t, "姓名", locales["zh-CN"]["name"])
 	testify.Equal(t, "年龄", locales["zh-CN"]["age"])
 }
+
+func TestLoadLocalesFromURL(t *testing.T) {
+	locales, err := LoadLocalesFromURL("https://raw.githubusercontent.com/go-zoox/i18n/master/tests/locales.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testify.Equal(t, "en-US", locales["en-US"]["en-US"])
+	testify.Equal(t, "zh-CN", locales["en-US"]["zh-CN"])
+	testify.Equal(t, "name", locales["en-US"]["name"])
+	testify.Equal(t, "age", locales["en-US"]["age"])
+
+	testify.Equal(t, "英文", locales["zh-CN"]["en-US"])
+	testify.Equal(t, "中文", locales["zh-CN"]["zh-CN"])
+	testify.Equal(t, "姓名", locales["zh-CN"]["name"])
+	testify.Equal(t, "年龄", locales["zh-CN"]["age"])
+}
